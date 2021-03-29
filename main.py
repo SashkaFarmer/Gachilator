429b7bc2b64ce8bb5b8e68bd7fa6c48608c81447
SashkaFarmer <Dalossteam@mail.ru>
2021-03-29 14:05:52 +0300

    all files

---
+++ main.py
@@ -0,0 +1,266 @@
+from tkinter import *
+from tkinter import messagebox
+from math import sqrt
+from PIL import ImageTk, Image
+from tkinter import ttk
+from pictures import *
+from winsound import *
+
+PlaySound('sounds\\gachi_ost.wav', SND_FILENAME | SND_ASYNC | SND_LOOP)
+
+
+def add_digit(digit):
+    value = calc.get()
+    if value == '0':
+        value = value[1:]
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value+digit)
+    calc['state'] = DISABLED
+
+def add_operation(operation):
+    value = calc.get()
+    if value[-1] in '/*-+':
+        value = value[:-1]
+    elif '+' in value or '-' in value or '/' in value or '*' in value:
+        calculate()
+        value = calc.get()
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value+operation)
+    calc['state'] = DISABLED
+
+def add_float(float):
+    value = calc.get()
+    if value[-1] == float:
+        value = value[:-1]
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value+float)
+    calc['state'] = DISABLED
+
+def add_square():
+    value = calc.get()
+    try:
+       value = float(value)
+       value = sqrt(value)
+    except ValueError:
+        messagebox.showinfo('ОшибОчка', 'Сначала нажми равно или хз как ты сюда попал')
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value)
+    calc['state'] = DISABLED
+
+def add_pow():
+    value = calc.get()
+    try:
+       value = float(value)
+       value = (value**2)
+    except OverflowError:
+        messagebox.showinfo('ОшибОчка', 'Слишком больше число')
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value)
+    calc['state'] = DISABLED
+
+
+def calculate():
+    value = calc.get()
+    if value[-1] in '/*-+':
+        value = value+value[:-1]
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    try:
+        calc.insert(0, eval(value))
+    except ZeroDivisionError:
+        messagebox.showinfo('Ты чё?', 'На нолье вообще-то нельзя делить', icon='warning')
+        calc.insert(0, 0)
+    except SyntaxError:
+        messagebox.showinfo('Нельзя использовать две точки',
+                            'Я пока не знаю как это решить, поэтому пусть будет так')
+        calc.insert(0, 0)
+    calc['state'] = DISABLED
+
+def clear():
+    value = calc.get()
+    value = value[:-1]
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, value)
+    calc['state'] = DISABLED
+
+
+def clear_all():
+    calc['state'] = NORMAL
+    calc.delete(0, END)
+    calc.insert(0, 0)
+    calc['state'] = DISABLED
+
+def press_number(butt):
+    print(repr(butt.char))
+    if butt.char.isdigit():
+        add_digit(butt.char)
+    elif butt.char in '/*-+':
+        add_operation(butt.char)
+    elif butt.char == '\r':
+        calculate()
+
+
+root = Tk()
+root.title('♂Гачилятор♂')
+root.iconphoto(False, PhotoImage(file='pictures\\billy.png'))
+root['bg'] = '#78c6cf'
+root.geometry('450x600+1050+200')
+root.resizable(False, False)
+root.bind('<Key>', press_number)
+
+
+calc = Entry(root, justify=RIGHT, width=19, font=('ProductSans', 20))
+calc.insert(0, 0)
+calc.place(x=80, y=60)
+calc['state'] = DISABLED
+
+
+def song_on():
+    PlaySound('sounds\\gachi_ost.wav', SND_FILENAME | SND_ASYNC | SND_LOOP)
+
+def song_off():
+    PlaySound('sounds\\fuck_you.wav', SND_FILENAME | SND_ASYNC)
+
+var = IntVar()
+var.set(0)
+
+check_on = Radiobutton(text='ON', value=0, command=song_on, variable=var, bg='#78c6cf',
+                       activebackground='#78c6cf', font='ProductSans 10')
+check_on.place(x=5, y=570)
+
+check_off = Radiobutton(text='OFF', value=1, command=song_off, variable=var, bg='#78c6cf',
+                        activebackground='#78c6cf', font='ProductSans 10')
+check_off.place(x=55, y=570)
+
+
+# Я пытался засунуть эти кнопки в функции, что бы сократить код и не нарушать принцип DRY,
+# но в таком случае не отображаются картинки, пытался найти решение, но не смог
+
+image = ImageTk.PhotoImage(img)
+btn = Button(root, image=image, text='a^n', compound='center', command=add_pow,
+             fg='White', activeforeground='#78c6cf', font='ProductSans 19',
+             height=48, width=48, borderwidth=0)
+btn.place(x=80, y=120)
+
+image1 = ImageTk.PhotoImage(img1)
+btn1 = Button(root, image=image1, text='√', compound='center', command=add_square,
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn1.place(x=160, y=120)
+
+image2 = ImageTk.PhotoImage(img2)
+btn2 = Button(root, image=image2, text='CE', compound='center', command=clear,
+              fg='White', activeforeground='#78c6cf', font='ProductSans 23',
+              height=48, width=48, borderwidth=0)
+btn2.place(x=240, y=120)
+
+image3 = ImageTk.PhotoImage(img3)
+btn3 = Button(root, image=image3, text='AC', compound='center', command=clear_all,
+              fg='White', activeforeground='#78c6cf', font='ProductSans 23',
+              height=48, width=48, borderwidth=0)
+btn3.place(x=320, y=120)
+
+image4 = ImageTk.PhotoImage(img4)
+btn4 = Button(root, image=image4, text='7', compound='center', command=lambda: add_digit('7'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn4.place(x=80, y=200)
+
+image5 = ImageTk.PhotoImage(img5)
+btn5 = Button(root, image=image5, text='8', compound='center', command=lambda: add_digit('8'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn5.place(x=160, y=200)
+
+image6 = ImageTk.PhotoImage(img6)
+btn6 = Button(root, image=image6, text='9', compound='center', command=lambda: add_digit('9'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn6.place(x=240, y=200)
+
+image7 = ImageTk.PhotoImage(img7)
+btn7 = Button(root, image=image7, text='÷', compound='center', command=lambda: add_operation('/'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn7.place(x=320, y=200)
+
+image8 = ImageTk.PhotoImage(img8)
+btn8 = Button(root, image=image8, text='4', compound='center', command=lambda: add_digit('4'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn8.place(x=80, y=280)
+
+image9 = ImageTk.PhotoImage(img9)
+btn9 = Button(root, image=image9, text='5', compound='center', command=lambda: add_digit('5'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+              height=48, width=48, borderwidth=0)
+btn9.place(x=160, y=280)
+
+image10 = ImageTk.PhotoImage(img10)
+btn10 = Button(root, image=image10, text='6', compound='center', command=lambda: add_digit('6'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn10.place(x=240, y=280)
+
+image11 = ImageTk.PhotoImage(img11)
+btn11 = Button(root, image=image11, text='×', compound='center', command=lambda: add_operation('*'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn11.place(x=320, y=280)
+
+image12 = ImageTk.PhotoImage(img12)
+btn12 = Button(root, image=image12, text='1', compound='center', command=lambda: add_digit('1'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn12.place(x=80, y=360)
+
+image13 = ImageTk.PhotoImage(img13)
+btn13 = Button(root, image=image13, text='2', compound='center', command=lambda: add_digit('2'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn13.place(x=160, y=360)
+
+image14 = ImageTk.PhotoImage(img14)
+btn14 = Button(root, image=image14, text='3', compound='center',command=lambda: add_digit('3'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn14.place(x=240, y=360)
+
+image15 = ImageTk.PhotoImage(img15)
+btn15 = Button(root, image=image15, text='–', compound='center', command=lambda: add_operation('-'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn15.place(x=320, y=360)
+
+image16 = ImageTk.PhotoImage(img16)
+btn16 = Button(root, image=image16, text='0', compound='center', command=lambda: add_digit('0'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn16.place(x=80, y=440)
+
+image17 = ImageTk.PhotoImage(img17)
+btn17 = Button(root, image=image17, text='•', compound='center', command=lambda: add_float('.'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn17.place(x=160, y=440)
+
+image18 = ImageTk.PhotoImage(img18)
+btn18 = Button(root, image=image18, text='=', compound='center', command=calculate,
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn18.place(x=240, y=440)
+
+image19 = ImageTk.PhotoImage(img19)
+btn19 = Button(root, image=image19, text='+', compound='center', command=lambda: add_operation('+'),
+              fg='White', activeforeground='#78c6cf', font='ProductSans 28',
+               height=48, width=48, borderwidth=0)
+btn19.place(x=320, y=440)
+
+
+root.mainloop()
---
+++ pictures.py
@@ -0,0 +1,64 @@
+from PIL import ImageTk, Image
+
+# Худший код в моей жизни, но я не смог засунуть этот код в функцию,
+# так как тогда не отображались изображения на кнопках, пытался найти решение, но всё бесполезно
+
+img = Image.open('pictures\\milos.jpg')
+img = img.resize((54, 54))
+
+img1 = Image.open('pictures\\billy1.jpg')
+img1 = img1.resize((54, 54))
+
+img2 = Image.open('pictures\\Van.jpg')
+img2 = img2.resize((54, 54))
+
+img3 = Image.open('pictures\\forsen.jpg')
+img3 = img3.resize((54, 54))
+
+img4 = Image.open('pictures\\gachi1.jpg')
+img4 = img4.resize((54, 54))
+
+img5 = Image.open('pictures\\gachi.jpg')
+img5 = img5.resize((54, 54))
+
+img6 = Image.open('pictures\\van1.jpg')
+img6 = img6.resize((54, 54))
+
+img7 = Image.open('pictures\\billy2.jpg')
+img7 = img7.resize((54, 54))
+
+img8 = Image.open('pictures\\billy3.jpg')
+img8 = img8.resize((54, 54))
+
+img9 = Image.open('pictures\\billy4.jpeg')
+img9 = img9.resize((54, 54))
+
+img10 = Image.open('pictures\\gachi2.jpg')
+img10 = img10.resize((54, 54))
+
+img11 = Image.open('pictures\\billy5.jpg')
+img11 = img11.resize((54, 54))
+
+img12 = Image.open('pictures\\van2.jpg')
+img12 = img12.resize((54, 54))
+
+img13 = Image.open('pictures\\billy6.jpg')
+img13 = img13.resize((54, 54))
+
+img14 = Image.open('pictures\\milos1.jpg')
+img14 = img14.resize((54, 54))
+
+img15 = Image.open('pictures\\van3.jpg')
+img15 = img15.resize((54, 54))
+
+img16 = Image.open('pictures\\billy7.jpg')
+img16 = img16.resize((54, 54))
+
+img17 = Image.open('pictures\\milos2.jpg')
+img17 = img17.resize((54, 54))
+
+img18 = Image.open('pictures\\gachi3.jpg')
+img18 = img18.resize((54, 54))
+
+img19 = Image.open('pictures\\milos3.jpg')
+img19 = img19.resize((54, 54))
